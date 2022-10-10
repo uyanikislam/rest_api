20e92cf88d3c8b76adb77af87459a477c4e15d4f
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-10 15:47:42 +0300

    models, thunder client

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,23 @@ const express=require("express");
 const router= express.Router();
 
 router.get("/", (req,res) =>{
-    res.send("products");
+    res.send("fetch product list");
+});
+
+router.get("/:id", (req,res) =>{
+    res.send(`fetch product ${req.params.id}`);
+});
+
+router.post("/", (req,res) =>{
+    res.send("create product");
+});
+
+router.put("/:id", (req,res) =>{
+    res.send(`update product ${req.params.id}`);
+});
+
+router.delete("/:id", (req,res) =>{
+    res.send(`delete product ${req.params.id}`);
 });
 
 
