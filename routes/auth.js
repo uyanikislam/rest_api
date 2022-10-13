d3fcbd29d7e49e65989bad57204d288e4f1474be
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-13 22:00:25 +0300

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,36 @@
 const express=require("express");
 const User= require("../models/User")
+const Joi=require("@hapi/joi")
+const bcrypt = require("bcryptjs")
+
+
+const registerSchema=Joi.object({
+    name: Joi.string().required().min(3).max(255),
+    email: Joi.string().required().email().min(6).max(255), 
+    password: Joi.string().required().min(6).max(255),
+})
+
 const router= express.Router();
 
 
-router.post('/register', (req,res)=>{
-    const{name, email, password} = req.body;
-    const user= new User({name, email, password});
-    user.save().then((user) =>{res.json(user)}).catch((err) =>{res.json(err)});
+router.post("/register", (req,res)=>{
+    
+    const  {error} = registerSchema.validate(req.body);
+    if (error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+     const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const user= new User(req.body);
+    user
+    .save()
+    .then((user) =>{
+        res.json(user)})
+    .catch((err) =>{
+        res.json(err)});
 });
 
 router.post('/login', (req,res)=>{
